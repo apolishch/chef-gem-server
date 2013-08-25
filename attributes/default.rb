c2187d8c89276ee20b4d50c5ce74c80b2b71d845
Abraham <abe@gobalto.com>
2013-08-25 03:31:08 -0700

    Adding systemd and init.d based templates. Sanitizeing platform_family list. Installing kitchen and introducing .gitignore

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,30 @@
 default['gem_server']['port']=8808
+default['gem_server']['service_description']="Custom Ruby Gem Server"
+default['gem_server']['gem_path'] = "/usr/bin/gem"
 case node['platform_family']
-when 'mac_os_x'
+when "mac_os_x"
   default['gem_server']['gem_dir']='/usr/share/gem_server/gems'
+
+when "arch", "suse", "fedora"
+
+when "debian"
+  default['gem_server']['gem_run_levels'] = [3,5]
+  default['gem_server']['not_gem_run_levels'] = [0,1,2,6]
+
+when "rhel"
+
+when "gentoo"
+
+when "slackware"
+
+when "openbsd"
+
+when "solaris2"
+
+when "omnios"
+
+when "windows"
+
+else
+
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,12 @@
 #
 
 if node[:gem_server][:enabled]
+  @template_variables = {
+      gem_server_gem_dir: node[:gem_server][:gem_dir],
+      gem_server_port: node[:gem_server][:port],
+      gem_server_description: node[:gem_server][:service_description],
+      gem_path: node[:gem_server][:gem_path]
+  }
 
   service "gem_server" do
     action :enable
@@ -22,19 +28,47 @@ if node[:gem_server][:enabled]
   end
 
   case node['platform_family']
-  when 'mac_os_x'
+  when "mac_os_x"
     template "org.rubyforge.rubygems.server.plist" do
       path "/Library/LaunchAgents"
       source "org.rubyforge.rubygems.server.plist.erb"
       owner "root"
       group "root"
       mode "0644"
-      variables({
-        gem_server_gem_dir: node[:gem_server][:gem_dir],
-        gem_server_port: node[:gem_server][:port]
-      })
-      notifies :restart, resources(:service => "gem_server")
+      variables(@template_variables)
+      notifies :restart, resources(service: "gem_server")
     end
+
+  when "arch", "suse", "fedora"
+    template "gem_server.service" do
+      path "/etc/system/systemd"
+      source "gem_server_systemd.service.erb"
+      owner "root"
+      group "root"
+      mode "0644"
+      variables(@template_variables)
+      notifies :restart, resources(service: "gem_server")
+end
+
+  when "debian"
+
+  when "rhel"
+
+  when "gentoo"
+
+  when "slackware"
+
+  when "openbsd"
+
+  when "solaris2"
+
+  when "omnios"
+
+  when "windows"
+
+  else
+
   end
+
 
 end
